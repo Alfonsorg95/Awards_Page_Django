4cef122b292f8ea031efc0d169d3eb5be8850439
Alfonso Rodriguez <alfonsorg95@gmail.com>
2023-04-26 22:47:06 +0200

    Polls app working with a frontend showing polls, options and results.

--- AwardsPageapp/polls/urls.py
+++ AwardsPageapp/polls/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="question"),
-    path("<int:question_id>/results", views.result, name="question"),
-    path("<int:question_id>/vote", views.vote, name="question"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results", views.ResultlsView.as_view(), name="results"),
+    path("<int:question_id>/vote", views.vote, name="vote"),
 ]
--- AwardsPageapp/polls/views.py
+++ AwardsPageapp/polls/views.py
@@ -1,18 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.views import generic
+
+from .models import Question, Choice
 
 
-def index(request):
-    return HttpResponse("Hello World")
+# def index(request):
+#     latest_question_list = Question.objects.all()
+#     return render(
+#         request,
+#         "polls/index.html",
+#         {"latest_question_list": latest_question_list},
+#     )
 
 
-def detail(request, question_id):
-    return HttpResponse(f"Question number: {question_id}")
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(
+#         request,
+#         "polls/detail.html",
+#         {
+#             "question": question,
+#         },
+#     )
 
 
-def result(request, question_id):
-    return HttpResponse(f"Results of question number: {question_id}")
+# def result(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(
+#         request,
+#         "polls/results.html",
+#         {
+#             "question": question,
+#         },
+#     )
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-publication_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultlsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Voting to question number: {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect("polls:results", question_id)
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "Must select a valid option",
+            },
+        )
